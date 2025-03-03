4a51e8df5807c58ee0e843844e61a2e81c707c90
jouider <jouider.abdellah@gmail.com>
2025-03-03 03:28:58 +0000

    mofidy init.sh to fix deployement

--- git-hooks/init.sh
+++ git-hooks/init.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-cp git-hooks/pre-commit .git/hooks/pre-commit
+
+# Check if the CI environment variable is set (usually set by CI/CD platforms)
+if [ "$CI" != "true" ]; then
+  # Only run the script if we are not in a CI/CD environment
+  cp git-hooks/pre-commit .git/hooks/pre-commit
+else
+  echo "Skipping pre-commit hook installation in CI/CD environment."
+fi
