1f8d1973d7e89d250e8df7311518f1f78b460860
jouider <jouider.abdellah@gmail.com>
2025-03-03 03:57:41 +0000

    add build script for deployment

--- git-hooks/init.sh
+++ git-hooks/init.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-
-# Check if the CI environment variable is set (usually set by CI/CD platforms)
-if [ "$CI" != "true" ]; then
-  # Only run the script if we are not in a CI/CD environment
-  cp git-hooks/pre-commit .git/hooks/pre-commit
-else
-  echo "Skipping pre-commit hook installation in CI/CD environment."
-fi
+cp git-hooks/pre-commit .git/hooks/pre-commit
